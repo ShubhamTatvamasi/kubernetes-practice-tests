96a06135d0ae04fa4f020f7576968cb77729281f
Shubham Tatvamasi <shubhamtatvamasi@gmail.com>
2020-02-24 14:27:52 +0530

    Update test.sh

--- test.sh
+++ test.sh
@@ -14,9 +14,9 @@ do
   test_url=$(curl -s ${url} | html2text | tail -n3)
 
   if [ "${scenario_not_found}" != "${test_url}" ]; then
-      echo -e "\033[0;32mTest Passed ${test_number} : ${url##*2/} "
+      echo -e "\033[0;32mTest Passed ${test_number} : ${url##*2/}"
   else
-      echo -e "\033[0;31mTest failed ${test_number} : ${url##*2/} "
+      echo -e "\033[0;31mTest failed ${test_number} : ${url##*2/}"
       exit_status=1
   fi
 
