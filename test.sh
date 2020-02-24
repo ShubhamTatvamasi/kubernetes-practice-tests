dc85011818cc1c9ab3d1741f265b4adff754ee54
Shubham Tatvamasi <shubhamtatvamasi@gmail.com>
2020-02-24 13:21:45 +0530

    modified:   test.sh

--- test.sh
+++ test.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 exit_status=0
-testNumber=0
+test_number=0
 
 scenario_not_found=$(curl -s \
 https://katacoda.com/embed/mmumshad2/scenario-not-found/ \
@@ -10,13 +10,13 @@ https://katacoda.com/embed/mmumshad2/scenario-not-found/ \
 while read url
 do
 
-  let "testNumber++"
-  testUrl=$(curl -s ${url} | html2text | tail -n3)
+  let "test_number++"
+  test_url=$(curl -s ${url} | html2text | tail -n3)
 
-  if [ "${scenario_not_found}" != "${testUrl}" ]; then
-      echo -e "\033[0;32mTest Passed ${testNumber} : ${url##*2/} "
+  if [ "${scenario_not_found}" != "${test_url}" ]; then
+      echo -e "\033[0;32mTest Passed ${test_number} : ${url##*2/} "
   else
-      echo -e "\033[0;31mTest failed ${testNumber} : ${url##*2/} "
+      echo -e "\033[0;31mTest failed ${test_number} : ${url##*2/} "
       exit_status=1
   fi
 
