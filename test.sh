a102c257d3ea6d39a94cb5c573b49f7974bcd72b
Shubham Tatvamasi <shubhamtatvamasi@gmail.com>
2020-05-08 02:49:52 +0530

    nonce fix again (#5)
    * new file:   scenarios
    
    * modified:   test.sh
    
    * new file:   index.html

--- test.sh
+++ test.sh
@@ -3,24 +3,33 @@
 exit_status=0
 test_number=0
 
+function generate_nonce {
+  curl -s "https://katacoda.com/api/embed/nonce"
+}
+
+author="https://katacoda.com/embed/mmumshad2/"
+fake_scenario="scenario-not-found"
+flags="?embed&url&host=kodekloud.com&nonce="
+
 scenario_not_found=$(curl -s \
-  "https://katacoda.com/embed/mmumshad2/scenario-not-found/?embed&url&host=kodekloud.com" \
+  "${author}${fake_scenario}${flags}$(generate_nonce)" \
   | html2text | tail -3 | head -1)
 
-while read url
+while read scenario
 do
 
   let "test_number++"
-  test_url=$(curl -s ${url} | html2text | tail -3 | head -1)
-  test_name=$(echo ${url##*d2/} | rev | cut -c 31- | rev)
 
-  if [ "${scenario_not_found}" != "${test_url}" ]; then
-      echo -e "\033[0;32mTest Passed ${test_number} : ${test_name}"
+  test_scenario=$(curl -s "${author}${scenario}${flags}$(generate_nonce)" \
+    | html2text | tail -3 | head -1)
+
+  if [ "${scenario_not_found}" != "${test_scenario}" ]; then
+      echo -e "\033[0;32mTest Passed ${test_number} : ${scenario}"
   else
-      echo -e "\033[0;31mTest failed ${test_number} : ${test_name}"
+      echo -e "\033[0;31mTest failed ${test_number} : ${scenario}"
       exit_status=1
   fi
 
-done < urls
+done < scenarios
 
 exit "${exit_status}"
